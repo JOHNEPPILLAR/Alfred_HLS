efe41aec2d5e168f599ebca8b072d315aa345fb6
JOHNEPPILLAR <johneppillar@gmail.com>
2019-08-18 13:33:57 +0100

    Refactored RTSP to streams and safe to disk

--- api/stream/stream.js
+++ api/stream/stream.js
@@ -5,14 +5,12 @@ const Skills = require('restify-router').Router;
 const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
-const UUID = require('pure-uuid');
-const rimraf = require('rimraf');
-const ffmpeg = require('fluent-ffmpeg');
 
 /**
  * Import helper libraries
  */
 const serviceHelper = require('../../lib/helper.js');
+const RTSPRecorder = require('../../lib//RTSPRecorder.js');
 
 const skill = new Skills();
 const CONTENT_TYPE = {
@@ -20,94 +18,6 @@ const CONTENT_TYPE = {
   SEGMENT: 'video/MP2T',
   HTML: 'text/html',
 };
-
-function checkFileExists(filePath) {
-  const timeout = 90000; // 90 Seconds
-  return new Promise((resolve) => {
-    const timer = setTimeout(() => {
-      // eslint-disable-next-line no-use-before-define
-      watcher.close();
-      resolve(false);
-    }, timeout);
-
-    fs.access(filePath, fs.constants.R_OK, (err) => {
-      if (!err) {
-        clearTimeout(timer);
-        // eslint-disable-next-line no-use-before-define
-        watcher.close();
-        resolve(true);
-      }
-    });
-    const dir = path.dirname(filePath);
-    const basename = path.basename(filePath);
-
-    const watcher = fs.watch(dir, (eventType, filename) => {
-      if (filename === basename) {
-        clearTimeout(timer);
-        watcher.close();
-        resolve(true);
-      }
-    });
-  });
-}
-
-function removeTempFolder(removeUUID) {
-  try {
-    serviceHelper.log('trace', `Removing temp folder: ${removeUUID}`);
-    rimraf.sync(`streams/${removeUUID}`);
-  } catch (err) {
-    serviceHelper.log('error', err.message);
-  }
-}
-
-/**
- * @api {get} /stopStream
- * @apiName stopStream
- * @apiGroup Stream
- *
- * @apiSuccessExample {json} Success-Response:
- *   HTTPS/1.1 200 OK
- *   {
- *     data: true
- *   }
- *
- * @apiErrorExample {json} Error-Response:
- *   HTTPS/1.1 400 Bad Request
- *   {
- *     data: Error message
- *   }
- *
- */
-function removeStream(streamUUID) {
-  let foundStream = false;
-  global.streamsStore.forEach((item, index, object) => {
-    try {
-      if (item[0].format() === streamUUID) {
-        item[1].kill();
-        removeTempFolder(streamUUID);
-        object.splice(index, 1);
-        foundStream = true;
-      }
-    } catch (err) {
-      serviceHelper.log('error', err.message);
-    }
-  });
-  return foundStream;
-}
-
-async function stopStream(req, res, next) {
-  const { streamUUID } = req.query;
-  if (removeStream(streamUUID)) {
-    serviceHelper.log('trace', `Stopped stream: ${streamUUID}`);
-    serviceHelper.sendResponse(res, true, `Stopped stream: ${streamUUID}`);
-  } else {
-    serviceHelper.log('trace', `Unable to stop stream: ${streamUUID}`);
-    serviceHelper.sendResponse(res, true, `Unable to stop stream: ${streamUUID}`);
-  }
-  serviceHelper.log('info', `Streams active: ${global.streamsStore.length} `);
-  next();
-}
-skill.get('/stop', stopStream);
 
 /**
  * @api {get} /startStream
@@ -117,8 +27,8 @@ skill.get('/stop', stopStream);
  * @apiSuccessExample {json} Success-Response:
  *   HTTPS/1.1 200 OK
  *   {
- *     "data": "58d7b01e-2e3c-4d3a-9e09-75cbea5d44ee"
- *   }
+ *     "data": "recordings/cd1a0e08-9b08-4565-a2b6-a2756cf85e8b/cam.m3u8"
+*    }
  *
  * @apiErrorExample {json} Error-Response:
  *   HTTPS/1.1 400 Bad Request
@@ -128,87 +38,16 @@ skill.get('/stop', stopStream);
  *
  */
 async function startStream(req, res, next) {
-  let streamUUID;
-
-  try {
-    serviceHelper.log('trace', 'Create base streams folder if missing');
-    fs.mkdirSync('streams');
-    // eslint-disable-next-line no-empty
-  } catch (err) {}
-
-  try {
-    streamUUID = new UUID(4);
-    const folderPath = `streams/${streamUUID}`;
-    const fullFilePath = `${folderPath}/cam.m3u8`;
-
-    serviceHelper.log('trace', 'Create temp storage path');
-    fs.mkdirSync(folderPath);
-
-    let { camURL } = process.env;
-    if (process.env.Mock === 'true') {
-      serviceHelper.log('info', 'Mock mode enabled, using test file as stream');
-      camURL = process.env.mockCamURL;
-    }
-
-    // writeStream = childProcess.spawn
-
-    const writeStream = ffmpeg(camURL, { timeout: 432000 });
-    writeStream.addOptions([
-      '-profile:v baseline',
-      '-level: 3.0',
-      '-f hls',
-      '-hls_time 3',
-      '-hls_wrap 10',
-    ]);
-    writeStream
-      .output(fullFilePath)
-      .once('start', async () => {
-        serviceHelper.log('trace', 'Started converting');
-        global.streamsStore.push([streamUUID, writeStream]);
-        const streaming = await checkFileExists(fullFilePath);
-        if (streaming) {
-          serviceHelper.log(
-            'info',
-            `New stream started. Active streams: ${global.streamsStore.length}`,
-          );
-          serviceHelper.sendResponse(res, true, streamUUID.format());
-        } else {
-          serviceHelper.log('error', `Stream file check timeout: ${streamUUID.format()}`);
-          serviceHelper.sendResponse(res, false, 'Not able to start stream');
-          removeStream(streamUUID.format());
-        }
-        next();
-      })
-      .on('error', (err) => {
-        serviceHelper.log('error', `Stream ${streamUUID.format()} error: ${err.message}`);
-      })
-      .once('end', () => {
-        serviceHelper.log('trace', `Stream ended: ${streamUUID.format()}`);
-        serviceHelper.log(
-          'info',
-          `Stream ${streamUUID.format()} ended. Active streams: ${global.streamsStore.length}`,
-        );
-      })
-      .run();
-
-    // writeStream.unref();
-
-    // Timeout stream
-    setTimeout(() => {
-      serviceHelper.log('trace', `Timeout reached for stream: ${streamUUID.format()}`);
-      if (removeStream(streamUUID.format())) {
-        serviceHelper.log('info', `Tidy up old stream: ${streamUUID.format()}`);
-        serviceHelper.log('info', `${global.streamsStore.length} active stream(s)`);
-      } else {
-        serviceHelper.log('trace', `Stream not active: ${streamUUID.format()}`);
-      }
-    }, 600000); // 10 minutes
-  } catch (err) {
-    serviceHelper.log('error', err.message);
-    removeTempFolder(streamUUID.format());
-    serviceHelper.sendResponse(res, false, err.message);
-    next();
-  }
+  serviceHelper.log('trace', `Start stream API called for url: ${req.url}`);
+  const rec = new RTSPRecorder({
+    url: process.env.camURL,
+    type: 'stream',
+    disableStreaming: true,
+    timeLimit: 600, // 10 minutes for each segmented video file
+  });
+  const streamUUID = rec.startRecording(); // Start Recording
+  serviceHelper.sendResponse(res, true, streamUUID);
+  next();
 }
 skill.get('/start', startStream);
 
@@ -268,13 +107,13 @@ async function getSegmentStream(req, res, next, filePath) {
 }
 
 async function playStream(req, res, next) {
-  serviceHelper.log('trace', `Create stream API called for url: ${req.url}`);
+  serviceHelper.log('trace', `Play stream API called for url: ${req.url}`);
 
   // Map path to files
   const fileName = path.basename(req.url).split('?')[0];
   const fileExt = path.extname(req.url);
   const streamFolder = req.url.split('/')[3];
-  const filePath = `streams/${streamFolder}/${fileName}`;
+  const filePath = `recordings/${streamFolder}/${fileName}`;
 
   // Check if stream is ready
   fs.exists(filePath, async (exists) => {
