ffceca37afd36e53dc339579f1139abacfeb8584
JP <johneppillar@gmail.com>
2020-10-25 22:00:37 +0000

    Refactor camera subscription events

--- db/views.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW vw_battery_data AS
-SELECT last("time", "time") AS "time", last(batteryLevel, "time") AS battery, deviceName || ' camera' as location, deviceID AS device
-FROM camera
---WHERE time > NOW() - interval '1 hour' 
-GROUP BY deviceID, deviceName
